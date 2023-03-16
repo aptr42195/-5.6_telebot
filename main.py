c2ea1183ed37cf5e46a8386cf667fe0663cb3dde
aptr42195 <aptraevmv@gmail.com>
2023-03-16 18:51:41 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+
+import telebot
+from config import TOKEN, keys
+from util import CriptoConverter, APIException
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=["start", "help"])
+def start_help(message):
+    text = "Для того, что бы начать работу с ботом, введите команду боту в следующем формате:\n" \
+           "<имя валюты> <в какую валюту перевести> <количество переводимой валюты> Увидеть список всех " \
+           "доступных валют:/values"
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=["values"])
+def values(message: telebot.types.Message):
+    text = "Доступные валюты:"
+    for key in keys.keys():
+        text = "\n".join((text, key,))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=["text", ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.lower().split()
+
+        if len(values) != 3:
+            raise APIException("Внимательно читайте инструкцию! Необходимо ввести три параметра. "
+                               "Наберите команду '/start' или нажмите --> /start")
+
+        quote, base, amount = values
+        total_base = CriptoConverter.get_price(quote, base, amount)
+
+    except APIException as e:
+        bot.reply_to(message, f"Ошибка пользователя.\n{e}")
+    except Exception as e:
+        bot.reply_to(message, f"Не удалось обработать команду.\n{e}")
+    else:
+        text = f"Цена {amount} {quote} в {base} - {total_base} "
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
---
+++ util.py
@@ -0,0 +1,37 @@
+import requests
+import json
+from config import keys
+
+
+class APIException(Exception):
+    pass
+
+
+class CriptoConverter:
+
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+
+        if quote == base:
+            raise APIException(f"Невозможно конвертировать одинаковые валюты {base}")
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f"Не удалось обработать валюту {quote}")
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f"Не удалось обработать валюту {base}")
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f"Не удалось обработать количество {amount}")
+
+        req = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}")
+        total_base = json.loads(req.content)[keys[base]] * amount
+
+
+        return round(total_base, 3)
